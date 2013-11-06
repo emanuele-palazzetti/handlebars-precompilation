43d234979b5ab43848c4dcef4b89052b81fc564d
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-11-06 12:17:37 +0100

    Initial script to initialize template engine context

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+if (typeof jQuery === "undefined") { throw new Error("This project requires jQuery") }
+
+;(function($, App) { "use strict";
+
+    // Build your template with automatical injection of Handlebar pre-compiled templates
+    var _nodeHtml = $('.js-marketing');
+
+    var _html = App.Templates.news({title: 'Happy news!', body: 'We are writing this section using a template engine with context!'})
+    _nodeHtml.html(_html);
+})(jQuery, App);
