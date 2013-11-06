c04c8fe559ea82d323a7c594dc3980cbd990c664
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-11-06 17:27:32 +0100

    Add each helper

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,11 @@ if (typeof jQuery === "undefined") { throw new Error("This project requires jQue
 ;(function($, App) { "use strict";
 
     // Build your template with automatical injection of Handlebar pre-compiled templates
-    var _nodeHtml = $('.js-marketing');
+    var _nodeHtml = $('.js-marketing'),
+        firstNews = new App.Model.News('Happy news!', 'We are writing this section using a template engine with context'),
+        secondNews = new App.Model.News('Some old news', 'Here you can store old information'),
+        news = [firstNews, secondNews];
 
-    var _html = App.Templates.news({title: 'Happy news!', body: 'We are writing this section using a template engine with context!'})
+    var _html = App.Templates.news({news: news});
     _nodeHtml.html(_html);
 })(jQuery, App);
---
+++ app/scripts/news.js
@@ -0,0 +1,9 @@
+;(function(App) { "use strict";
+
+    // News object
+    App.Model = {};
+    App.Model.News = function (title, content) {
+        this.title = title;
+        this.content = content;
+    }
+})(App);
